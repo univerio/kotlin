e05a64b255f8b8b7718b7e784b7bcffec385645f
Pavel V. Talanov <Pavel.Talanov@jetbrains.com>
2012-11-28 20:49:13 +0400

    Add missing public in stdlib

--- libraries/stdlib/src/kotlin/JLangJVM.kt
+++ libraries/stdlib/src/kotlin/JLangJVM.kt
@@ -8,7 +8,7 @@ import jet.runtime.Intrinsic
 public val <out T> T.javaClass : Class<T>
     [Intrinsic("kotlin.javaClass.property")] get() = (this as java.lang.Object).getClass() as Class<T>
 
-[Intrinsic("kotlin.javaClass.function")] fun <out T> javaClass() : Class<T> = null as Class<T>
+[Intrinsic("kotlin.javaClass.function")] public fun <out T> javaClass() : Class<T> = null as Class<T>
 
 
 /**
